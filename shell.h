87e03a8e9456bb86aab8b9ae780bfe33e6d576b4
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2022-12-10 14:26:07 +0000

    Shell Header File Update

--- shell.h
+++ shell.h
@@ -12,12 +12,38 @@
 #include <fcntl.h>
 #include <errno.h>
 
-
-
 /* Macros and prototype function*/
-/*for getline section */
+
+/* for read/write buffers */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+/* for command chaining */
+#define CMD_NORM	0
+#define CMD_OR		1
+#define CMD_AND		2
+#define CMD_CHAIN	3
+
+/* for convert_number() */
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
+
+/* 1 if using system getline() */
 #define USE_GETLINE 0
+#define USE_STRTOK 0
+
+#define HIST_FILE	".simple_shell_history"
+#define HIST_MAX	4096
+
+extern char **environ;
+
+/* getline functions */
 size_t input_buf(info_t *info, char **buf, size_t *len);
+ssize_t get_input(info_t *);
+int _getline(info_t *, char **, size_t *);
+void sigintHandler(int);
+ssize_t read_buf(info_t *info, char *buf, size_t *i);
 
 /**
  * struct liststr - singly linked list
@@ -75,6 +101,28 @@ typedef struct getinfo
 	int readfd;
 	int histcount;
 } info_t;
+
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+	0, 0, 0}
+
+/**
+ *struct builtin - contains a builtin string and related function
+ *@type: the builtin command flag
+ *@func: the function
+ */
+typedef struct builtin
+{
+	char *type;
+	int (*func)(info_t *);
+} builtin_table;
+
+
+/* _atoi function prototypes */
+int interactive(info_t *info);
+int is_delim(char c, char *delim);
+int _isalpha(int c);
+int _atoi(char *s);
 
 
 #endif /* END ALL PROCESS FOR SHELL_H */
